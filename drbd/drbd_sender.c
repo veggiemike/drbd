568e4189bedd6d9cfa91756026b44b964877cafe
Michael D Labriola <michael.d.labriola@gmail.com>
2021-03-07 17:31:32 -0500

    fix bd_contains usage for 5.11

--- drbd/drbd_sender.c
+++ drbd/drbd_sender.c
@@ -1949,14 +1949,16 @@ void drbd_resync_after_changed(struct drbd_device *device)
 void drbd_rs_controller_reset(struct drbd_peer_device *peer_device)
 {
 	struct fifo_buffer *plan;
-	struct hd_struct *part = &peer_device->device->ldev->backing_bdev->bd_contains->bd_disk->part0;
+	struct block_device *bdev = bdev_whole(peer_device->device->ldev->backing_bdev);
+	//struct hd_struct *part = &bdev->bd_disk->part0;
+	//struct hd_struct *part = &peer_device->device->ldev->backing_bdev->bd_contains->bd_disk->part0;
 
 	atomic_set(&peer_device->rs_sect_in, 0);
 	atomic_set(&peer_device->device->rs_sect_ev, 0);  /* FIXME: ??? */
 	peer_device->rs_last_mk_req_kt = ktime_get();
 	peer_device->rs_in_flight = 0;
-	peer_device->rs_last_events = (int)part_stat_read(part, sectors[0])
-		+ (int)part_stat_read(part, sectors[1]);
+	peer_device->rs_last_events = (int)part_stat_read(bdev, sectors[0])
+		+ (int)part_stat_read(bdev, sectors[1]);
 
 	/* Updating the RCU protected object in place is necessary since
 	   this function gets called from atomic context.
